06ffb66205381bfe9e1024ad648dbd1733f8caa7
Farhad Abasov <farhadabas@gmail.com>
2025-01-04 20:29:33 -0600

    updates

--- scripts/gen.py
+++ scripts/gen.py
@@ -1,6 +1,6 @@
 import json
 
-TOPICS_FILE = "lessons/lessons.json"
+LESSONS_FILE = "lessons/lessons.json"
 
 
 def _load_json(filepath):
@@ -10,17 +10,43 @@ def _load_json(filepath):
 
 
 def _load_topics():
-    return _load_json(TOPICS_FILE)
+    return _load_json(LESSONS_FILE)
 
 
 def _print_div():
     print("----------------------")
 
 
-def _print_topics():
-    topics = _load_topics()
-    pretty_topics = json.dumps(topics, indent=4)
-    print(pretty_topics)
+def _print_lessons():
+    lessons = _load_topics()
+    prettyLessons = json.dumps(lessons, indent=4)
+    print(prettyLessons)
+
+
+def _generate_lesson_content():
+    lessons = _load_topics()
+    for lid, language in enumerate(lessons):
+        _print_div()
+        name = language['name']
+        languageCode = language['language_code']
+        folder = language["folder"]
+        topics = language['topics']
+        print(f"({lid+1}/{len(lessons)}) {name}...")
+        print(f"    Language Code: {languageCode}")
+        print(f"    Folder: {folder}")
+        print("    Topics:")
+        for tidx, topic in enumerate(topics):
+            topicName = topic['name']
+            phrasesFile = topic['phrases_file']
+            fullPhrasesFile = f"{folder}/{phrasesFile}"
+            print(f"\t({tidx+1}/{len(topics)}) Generating content for {topicName} in {fullPhrasesFile}...")
+            
+            
+
+
+def _update_website():
+    pass
+
 
 if __name__ == '__main__':
     while True:
@@ -34,8 +60,11 @@ if __name__ == '__main__':
         _print_div()
 
         if choice == "1":
-            _print_topics()
-            
+            _print_lessons()
+        elif choice == "2":
+            _generate_lesson_content()
+        elif choice == "3":
+            _update_website()
         elif choice == "4":
             break
         else:
